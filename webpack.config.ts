4117b35996076a5684d33ec38ade5c337fae654b
EndureBlaze <hi@imyan.ren>
2022-02-16 15:38:53 +0800

    :recycle: Remove JavaScript

---
+++ webpack.config.ts
@@ -0,0 +1,37 @@
+import path = require("path");
+import webpack = require("webpack");
+
+const extensionConfig: webpack.Configuration = {
+  target: "node",
+  mode: "none",
+  entry: "./src/extension.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
+  },
+  externals: {
+    vscode: "commonjs vscode",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
+  devtool: "nosources-source-map",
+  infrastructureLogging: {
+    level: "log", // enables logging required for problem matchers
+  },
+};
+module.exports = [extensionConfig];
